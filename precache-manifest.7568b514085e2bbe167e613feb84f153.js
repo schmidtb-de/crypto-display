54b46a500cca401a439831a06b73890cabea6212
schmidtb <Benny@schmidtb.de>
2020-01-18 19:23:42 +0100

    Updates

---
+++ precache-manifest.7568b514085e2bbe167e613feb84f153.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716cb9844bae28166af8a4616168e771",
+    "url": "/schmidtb-de.github.io/index.html"
+  },
+  {
+    "revision": "eac494e5361e3723c75f",
+    "url": "/schmidtb-de.github.io/static/css/main.7bca4941.chunk.css"
+  },
+  {
+    "revision": "411fe7975a3fe2941ac4",
+    "url": "/schmidtb-de.github.io/static/js/2.3a718bbd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/schmidtb-de.github.io/static/js/2.3a718bbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eac494e5361e3723c75f",
+    "url": "/schmidtb-de.github.io/static/js/main.364293cc.chunk.js"
+  },
+  {
+    "revision": "52cac273bcfe5d2aafdd",
+    "url": "/schmidtb-de.github.io/static/js/runtime-main.89bbc2e5.js"
+  },
+  {
+    "revision": "2e480f7dd5298493855087dc1d6f3b35",
+    "url": "/schmidtb-de.github.io/static/media/bitcoin.2e480f7d.svg"
+  },
+  {
+    "revision": "2fec62be981d0c87cf558d663ae88603",
+    "url": "/schmidtb-de.github.io/static/media/ethereum.2fec62be.svg"
+  },
+  {
+    "revision": "b73f76ace7616578c93b88d5a0a69ae1",
+    "url": "/schmidtb-de.github.io/static/media/litecoin.b73f76ac.svg"
+  }
+]);
